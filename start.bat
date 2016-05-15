c55820f30344d4284211172561e7cb18ffefda20
Dumitru Glavan <dumitru.gl@gmail.com>
2016-05-15 22:24:12 +0200

    Add Win tools

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
 set NODE_ENV=production
-echo Starting the RFID Shop
+echo Starting the RFID Shop... Please access http://localhost:5000 in your browser.
 node app.js
-echo The shop was successfully started. Please access http://localhost:5000 in your browser.
---
+++ update.bat
@@ -0,0 +1,5 @@
+echo Updating the RFID Shop app...
+git pull
+npm install
+cp config.json.sample config.json
+echo The app was successfully updated.
